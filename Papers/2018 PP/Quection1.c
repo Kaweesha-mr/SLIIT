106fe21653f1ef7ded4df97abade111ecf74cacf
Kaweesha-mr <110246539+Kaweesha-mr@users.noreply.github.com>
2022-12-10 07:17:54 +0530

    update quection1.c
    code updated to without functions

--- Papers/2018 PP/Quection1.c
+++ Papers/2018 PP/Quection1.c
@@ -1,84 +1,69 @@
 #include<stdio.h>
-
-
-
-//declearing the functions
-float calculateH(int count);
-float calculateF(int count);
-float calculateM(int count);
-
-
 int main(void){
-	
-	//variables
-	char type;
-	int count=0,M=0,F=0,x=0,H=0;
-	float H_total,F_total,M_total;
-	
-	printf("Enter the course Type : ");
-	scanf(" %c%*c",&type);
-	
-	//loop to count values
-	while(count != 3 ){
-			
-		if(type == 'H' || type == 'h'){
-			H++;
-		}
-		else if(type == 'M' || type == 'm'){
-			M++;
-		}
-		else if(type == 'F' ||type == 'f'){
-			F++;
-		}
-		else{
-			
-			printf("Enter Valid Type \n");
-			
-			printf("Enter the course Type : ");
-			scanf(" %c%*c",&type);
-			continue;
-		}
-		
-		if(count != 2){
-			
-			printf("Enter the course Type : ");
-			scanf(" %c%*c",&type);
-		}
 
-		
-		count++;
-		
-	}
-	
-	//functions calling
-	H_total = calculateH(H);
-	M_total = calculateM(M);
-	F_total = calculateF(F);
-	
-	
-	//prnting the output
-	printf("Total Count of H : %d Amount  : %.2f\n",H,H_total);
-	printf("Total Count of M:  %d Amount  :%.2f\n",M,M_total);
-	printf("Total Count of F:  %d Amount  :%.2f\n",F,F_total);
-	
-	return 0;
-}
+    //declearing the variables
+    char type;
+    float H_total=0,M_total=0,F_total=0;
+    int H_count=0,M_count=0,F_count=0,count=0;
+
+    //getting the user input
+    printf("Enter Cource Type (to Terminate the programe enter 1) : ");
+    scanf(" %c",&type);
+
+    //loop to run until 100
+    while(count != 100 ){
+    
+    //checking conditions
+    if(type == 'H' || type == 'h'){
+        H_total = H_total + 1500;
+        H_count = H_count + 1;
+    }
+    else if (type == 'M' || type == 'm'){
+        M_total = M_total + 2000;
+        M_count = M_count + 1;
+
+    }
+    else if(type == 'F' || type == 'f'){
+        F_total = F_total + 2500;
+        F_count = F_count + 1;
+
+    }
+    else{
+        if(type == '1'){
+            break;
+        }
+        else{
+
+            //if user enters a invalid input this will reask the user without going to the next round of the loop
+            printf("Enter Valid data type\n");
+            
+            printf("Enter Cource Type (to Terminate the programe enter 1) : ");
+            scanf(" %c",&type);
+            continue;
+        }
+        
+    }
+
+    printf("Enter Cource Type (to Terminate the programe enter 1) : ");
+    scanf(" %c",&type);
+    count = count+1;//increment variable
+    
+    }
 
 
-float calculateH(int count){
-	
-	return 1500*count;
-	
-}
-float calculateF(int count){
-	
-	return 2500*count;
-	
-}
-float calculateM(int count){
-	
-	return 2000*count;
-	
+    //printing user inputs count of each type and total registraton fee of each type
+    printf("Count of H course Type : %d\n",H_count);
+    printf("Total Registration Fee for type H : %.2f\n",H_total);
+
+    printf("Count of M course Type : %d\n",M_count);
+    printf("Total Registration Fee for type M : %.2f\n",M_total);
+
+    printf("Count of F course Type : %d\n",F_count);
+    printf("Total Registration Fee for type F : %.2f\n",F_total);
+
+
+
+    return 0;
 }
 
 
