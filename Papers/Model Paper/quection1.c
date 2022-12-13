3aab2637b34e09fb8fef98aa3e4723d9120d4cfa
Kaweesha-mr <110246539+Kaweesha-mr@users.noreply.github.com>
2022-12-13 14:21:38 +0530

    Ip Model Paper

---
+++ Papers/Model Paper/quection1.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+
+int main(void){
+
+    //variable declear
+    char type,method,count;
+    int num_Of_Rooms,num__Of_Nights;
+    float total=0;
+
+    //decoration
+    printf("\n\t******---Blue Ocean---******");
+    printf("\n\t    -----Welcome..-----\n");
+
+    //loop to re ask the user
+    do{
+
+    //taking user inputs
+    printf("\n   >Enter Room Type : ");
+    scanf(" %c",&type);
+
+   
+    //checking the condition if user enters a wrong room type
+    if(!(type == 'D' || type =='S' || type =='C' ||type == 'E' ||type == 'P')){
+        printf("Please Enter Valid Room Type  \n");
+        return -1;
+    }
+
+    printf("\n   >Enter Number of Rooms : ");
+    scanf("%d",&num_Of_Rooms);
+
+    printf("\n   >Enter Number Of nights :");
+    scanf("%d",&num__Of_Nights);
+
+    printf("\n   >Enter Paying Method( M - Money /C - card)");
+    scanf(" %c",&method);
+
+
+    //calculating the total for user requirements
+    if(type == 'D'){
+        total = total + 31000.00*num_Of_Rooms*num__Of_Nights;
+
+    }
+    else if(type == 'S'){
+        total = total + 35000.00*num_Of_Rooms*num__Of_Nights;
+
+    }
+    else if(type == 'C'){
+        total = total + 50000.00*num_Of_Rooms*num__Of_Nights;
+        
+    }
+    else if(type == 'E'){
+        total = total + 75000.00*num_Of_Rooms*num__Of_Nights;
+        
+    }
+    else if(type == 'P'){
+        total = total + 100000.00*num_Of_Rooms*num__Of_Nights;
+        
+    }
+    if(method == 'C'){
+
+        total = total - total*0.1;
+    }
+
+    //printing the total for selected item
+    printf("Total : %.2f\n",total);
+
+    //prompt the user to ask for another round
+    printf("Do you Want to continue (Yes - Y / No - N) : ");
+    scanf(" %c",&count);
+
+
+    }while(!(count == 'N' || count == 'n'));
+    
+    return 0;
+}
